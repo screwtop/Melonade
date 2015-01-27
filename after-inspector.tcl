b7ab87405d80ab9dadec0c7365c88c11c65495ed
Chris Edwards <screwtop.github@ripples.dyndns.org>
2015-01-28 11:12:50 +1300

    - Handle separate type and script body returned by [after info].  - Added sensible column sizes.

--- after-inspector.tcl
+++ after-inspector.tcl
@@ -1,18 +1,19 @@
 # Browser/inspector for actions scheduled by [after]
 
-toplevel .afters; wm title .afters {[after] Schedules}
-ttk::treeview .afters.tree -columns {Script} -selectmode browse
+toplevel .afters; wm title .afters {[after] Events Scheduled}
+ttk::treeview .afters.tree -columns {Type Script} -selectmode browse
 pack .afters.tree -expand 1 -fill both
-.afters.tree heading #0 -text after
-.afters.tree heading #1 -text Script
+.afters.tree heading #0 -text ID; .afters.tree column #0 -minwidth 100 -width 100 -stretch 0
+.afters.tree heading #1 -text Type; .afters.tree column #1 -minwidth 80 -width 80 -stretch 0
+.afters.tree heading #2 -text Script
 
 proc refresh_after_tree {} {
 	# TODO: wasn't there a simpler way to do this that I found, avoiding the loop?
 	foreach after [.afters.tree children {}] {.afters.tree delete $after}
 	foreach after [lsort [after info]] {
 		set ::after_treeview_node($after) [.afters.tree insert {} end -text $after]
-		foreach {body} [after info $after] {
-			.afters.tree insert $::after_treeview_node($after) end -values [list $body]
+		foreach {script type} [after info $after] {
+			.afters.tree insert $::after_treeview_node($after) end -values [list $type $script]
 		}
 	}
 }
